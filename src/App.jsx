efbef019c8314bb9c5ab4b0c7d51275e08e96907
biddut mia <test@gamil.com>
2023-10-15 08:25:13 -0700

    one

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,5 @@
-import store from "./store";
-import { useSelector, useDispatch } from "react-redux";
 const App = () => {
-  const data = useSelector((state) => state);
-
-  const dispatch = useDispatch((dispatch) => dispatch);
-
-  return (
-    <div>
-      <div>count {data} </div>
-      <button
-        onClick={() => {
-          dispatch({ type: "add" });
-        }}
-      >
-        increment
-      </button>
-      <button
-        onClick={() => {
-          dispatch({ type: "sub" });
-        }}
-      >
-        decrement
-      </button>
-    </div>
-  );
+  return <div>this is a test project</div>;
 };
 
 export default App;
--- src/store/index.js
+++
@@ -1,7 +0,0 @@
-import { createStore } from "redux";
-import reducer from "./reducer";
-
-const init = 0;
-const store = createStore(reducer, init);
-
-export default store;
